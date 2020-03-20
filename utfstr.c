6eac563a5528ef2821350010405b5aa18d755fc7
sean <phonologus@gmail.com>
2020-03-20 22:12:58 +0000

    utfstr.c: added to repo.

---
+++ utfstr.c
@@ -0,0 +1,34 @@
+/*
+ * unicode string literals
+ */
+
+int utfstr_nl[]={'\n','\0'};
+int utfstr_nul[]={'\0'};
+
+int utfstr_whitespacequotes[]={' ','"','\t','\n','\0'};
+int utfstr_sharkteeth[]={'^','^','^','^','^','\0'};
+int utfstr_eqgtlt[]={'=','>','<','\0'};
+int utfstr_rbracewhitespace[]={'}',' ','\t','\n','\0'};
+int utfstr_starEOFstar[]={'[','*','E','O','F','*',']','\0'};
+int utfstr_bfFlprz[]={'b','f','F','l','p','r','z','!','\0'};
+int utfstr_whitespace[]={' ','\t','\n','\0'};
+int utfstr_shriek[]={'!','\0'};
+int utfstr_querypipe[]={'?','|','\0'};
+int utfstr_queryo[]={'?','o','\0'};
+int utfstr_cantforkqueryshriek[]={'C','a','n','\'','t',' ',
+                              'f','o','r','k','\n','?','!','\0'};
+int utfstr_cantfork[]={'C','a','n','\'','t',' ', 'f','o','r','k','\0'};
+int utfstr_ft_[]={'f','t',' ','\0'};
+int utfstr_p[]={'p','\0'};
+int utfstr_fnl[]={'f','\n','\0'};
+int utfstr_colonaq[]={':','a','q','\0'};
+int utfstr_qedhup[]={'q','e','d','.','h','u','p','\0'};
+int utfstr_br[]={'b','~',' ','r','\n','\\','b','~','\n','\0'};
+int utfstr_queryi[]={'?','i','\0'};
+int utfstr_suffix[]={'\377','\n','p','P','l','L',' ','\t','\0'};
+int utfstr_bignl[]={'\\','N','\0'};
+int utfstr_slu[]={'s','l','u','\0'};
+int utfstr_q[]={'q','\0'};
+int utfstr_rhsa[]={'/','&','^','\n','\\','1','2','3','4',
+                   '5','6','7','8','9','\0'};
+int utfstr_rhsb[]={'/','\\','\n','\0'};
